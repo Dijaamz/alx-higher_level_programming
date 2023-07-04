363a5291474768f2c0e5f2c6b697503c8601f039
Dijaamz <khadija17amzoug@gmail.com>
2023-07-04 16:11:02 -0700

    object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(s=set((0,))):
-    s.add(s.pop() + 1)
-    return ', '.join('Holberton' for _ in range(next(iter(s))))
+def magic_string(l=[]):
+    l += ["BestSchool"]
+    return ", ".join(l)
